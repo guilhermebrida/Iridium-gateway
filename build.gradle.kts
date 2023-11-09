95e47f5086331c65486da41ae14947c96e940e28
guilhermebrida <43304919+guilhermebrida@users.noreply.github.com>
2023-11-09 17:37:57 -0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.postgresql:postgresql:42.2.24")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,82 @@
+import java.sql.DriverManager
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+import java.time.LocalDateTime
+
+fun receiveMessage() {
+    val porta = 9876
+    val tamanhoBuffer = 1024
+    Thread {
+        try {
+
+            val socket = DatagramSocket(porta)
+            val buffer = ByteArray(tamanhoBuffer)
+
+            val pacote = DatagramPacket(buffer, buffer.size)
+            println("Aguardando pacote...")
+            socket.receive(pacote)
+
+
+            val mensagemRecebida = String(pacote.data, 0, pacote.length)
+            println("Mensagem recebida: $mensagemRecebida")
+            if (mensagemRecebida.isNotEmpty()){
+                insertIridiumMessage(mensagemRecebida)
+            }
+
+
+            //socket.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }.start()
+}
+fun insertIridiumMessage(message : String) {
+    val jdbcUrl = "jdbc:postgresql://18.231.44.167:5432/postgres"
+    val username = "postgres"
+    val password = "postgres"
+
+
+    try {
+        Class.forName("org.postgresql.Driver")
+
+        // Estabelecer a conexão com o banco de dados
+        val connection = DriverManager.getConnection(jdbcUrl, username, password)
+        val statement = connection.createStatement()
+        //select no banco
+        val resultSet = statement.executeQuery("SELECT * FROM iridium")
+        while (resultSet.next()) {
+            val column1Value = resultSet.getString("MESSAGE")
+            val column2Value = resultSet.getString("reception_datetime")
+            // Faça algo com os valores recuperados, por exemplo, imprima-os na tela
+            println("MESSAGE: $column1Value, RECEPTION_DATETIME: $column2Value")
+        }
+
+        //insert no banco
+        val insertQuery = "INSERT INTO iridium (\"MESSAGE\", reception_datetime) VALUES (?, ?)"
+        val preparedStatement = connection.prepareStatement(insertQuery)
+
+        preparedStatement.setString(1, message)
+        preparedStatement.setObject(2, LocalDateTime.now())
+
+        val rowsAffected = preparedStatement.executeUpdate()
+
+        println("$rowsAffected linha(s) afetada(s).")
+
+        // Fechar a conexão e o statement
+//        preparedStatement.close()
+//        connection.close()
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+fun main() {
+//    insertIridiumMessage("testando")
+    receiveMessage()
+}
+
+
+
